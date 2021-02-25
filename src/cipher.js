7d5ae88a99941e86c1420ea248d6c73331703983
Vanessa Cuenca <vanycuenca@gmail.com>
2021-02-25 11:59:14 -0300

    Proyecto pre final

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
   // ...
+ 
+encode (escalones, nuevoMensaje){
+
+  let correr = "";
+
+  if (nuevoMensaje == "" || nuevoMensaje == 0 ||nuevoMensaje == null || nuevoMensaje == [] ) {
+    throw new TypeError("caracter no permitido");
+
+ }
+  
+  
+  for (let i = 0; i < nuevoMensaje.length; i++) {
+    
+      let ascii = nuevoMensaje.charCodeAt(i);
+      escalones = parseInt(escalones);
+      let escalones1 = (ascii - 65 + escalones) % 26 + 65;
+      correr += String.fromCharCode(escalones1);
+
+ }
+  return correr;
+},
+
+decode (escalones, nuevoMensaje){
+
+  let caminar = "";
+
+  if (nuevoMensaje == "" || nuevoMensaje == 0 ||nuevoMensaje == null || nuevoMensaje == [] ) {
+    throw new TypeError("caracter no permitido");
+
+  }
+  
+  for (let i = 0; i < nuevoMensaje.length; i++) {
+      
+    let ascii = nuevoMensaje.charCodeAt(i);
+    escalones = parseInt(escalones);
+    let escalones1 = (ascii + 65 - escalones) % 26 + 65;
+    caminar += String.fromCharCode(escalones1);
+
+
+ }
+  return caminar;
+}
+
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// cambio de cuadro de mensaje
+
+let escribirMensaje = document.getElementById("mensaje1");
+let númeroEscalones = document.getElementById("escalones");
+
+document.getElementById("correr").addEventListener("click", function () {
+   
+      let mensaje = escribirMensaje.value;
+      let escalones = númeroEscalones.value;
+      
+         let nuevoMensaje = mensaje.toUpperCase();
+         try{
+         document.getElementById("mensaje1").value = cipher.encode(escalones, nuevoMensaje);
+      } catch(error){
+            alert(error.mensaje);
+         }
+
+           
+})
+
+document.getElementById("caminar").addEventListener("click", function () {
+   
+   let mensaje = escribirMensaje.value;
+   let escalones = númeroEscalones.value;
+   
+
+      let nuevoMensaje = mensaje.toUpperCase();
+      try{
+      document.getElementById("mensaje1").value = cipher.decode(escalones, nuevoMensaje);
+   } catch(error){
+      alert(error.mensaje);
+   }
+
+      
+})
+
+
+//console.log(cipher);
